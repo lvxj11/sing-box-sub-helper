d90d2b400241e80bdb318580925685a020b13454
lvxj11 <lvxj11@126.com>
2024-12-27 03:23:43 +0000

    v0.1.3 修改了一些配置提示

--- README.md
+++ README.md
@@ -6,12 +6,14 @@
 主要用于已经手搓了配置模板，又懒得搭建订阅转换后端的情况。
 
 ## 使用方法
-1. 从Releases下载sing-box-sub-helper合适的执行文件
-2. 运行sing-box-sub-helper，第一次运行会自动生成配置文件settings.ini
-3. 编辑配置文件settings.ini，修改订阅地址、节点模板、输出文件路径等参数
-4. 订阅地址和模板文件是必须的，输出文件可以使用默认
-5. 再次运行sing-box-sub-helper，生成sing-box配置文件。
-6. 运行sing-box run -c 配置文件。
+1. 从Releases下载sing-box-sub-helper合适的执行文件。
+2. 运行`sing-box-sub-helper`，第一次运行会自动生成配置文件settings.ini。
+3. 编辑配置文件settings.ini，修改订阅地址、节点模板、输出文件路径等参数。
+4. subscribeURL，templatePath 和 outputPath 是必须的。分别是订阅地址，模板文件和和输出文件路径。
+5. 如果修改了开始步骤，则根据步骤需要配置相应的数据源文件路径。
+6. 再次运行sing-box-sub-helper，生成sing-box配置文件。
+7. 运行`sing-box check -c "配置文件"`检查配置文件，或`sing-box run -c "配置文件"`直接执行。
+8. 后台运行或添加到系统服务方法请自行google。
 
 ### 其他说明
 1. 暂时只支持一个订阅链接
--- packages/get-config.go
+++ packages/get-config.go
@@ -46,11 +46,11 @@ func GetSettings() (Settings, error) {
 }
 
 // 读取ini格式配置
-func readIniConfig(settingsPath string, config Settings) (Settings, error) {
+func readIniConfig(settingsPath string, settings Settings) (Settings, error) {
 	settingBytes, err := os.ReadFile(settingsPath)
 	if err != nil {
 		fmt.Println("读取settings.ini失败:", err)
-		return config, err
+		return settings, err
 	}
 	// 遍历每行，兼容windows和linux换行符
 	lines := strings.Split(strings.ReplaceAll(string(settingBytes), "\r\n", "\n"), "\n")
@@ -73,35 +73,48 @@ func readIniConfig(settingsPath string, config Settings) (Settings, error) {
 		switch key {
 		case "subscribeURL":
 			if isValidUrl(value) {
-				config.SubscribeURL = value
+				settings.SubscribeURL = value
+			} else {
+				return settings, fmt.Errorf("订阅地址验证错误：%s", value)
 			}
 		case "base64File":
 			if isValidPath(value) {
-				config.Base64File = value
+				settings.Base64File = value
 			}
 		case "tempListPath":
 			if isValidPath(value) {
-				config.TempListPath = value
+				settings.TempListPath = value
 			}
 		case "tempJsonPath":
 			if isValidPath(value) {
-				config.TempJsonPath = value
+				settings.TempJsonPath = value
 			}
 		case "templatePath":
 			if isValidPath(value) {
-				config.TemplatePath = value
+				settings.TemplatePath = value
+			} else {
+				return settings, fmt.Errorf("模板文件路径验证错误：%s", value)
 			}
 		case "excludeKeywords":
-			config.Filter = []Filter{{Action: "exclude", Keywords: []string{value}}}
+			settings.Filter = []Filter{{Action: "exclude", Keywords: []string{value}}}
 		case "outputPath":
 			if isValidPath(value) {
-				config.OutputPath = value
+				settings.OutputPath = value
 			}
 		case "startStep":
-			config.StartStep = strToInt(value)
+			settings.StartStep = strToInt(value)
+			if settings.StartStep != 0 {
+				// 使用 ANSI 转义码显示红色并加粗
+				fmt.Println("\033[1;31m检测到修改了默认步骤，请确保相应步骤所需数据存在并正确配置路径。\033[0m")
+				if settings.StartStep > 4 {
+					fmt.Println("检测到开始步骤大于4，将自动重置为4。")
+					settings.StartStep = 4
+				}
+				fmt.Println("开始步骤为：", settings.StartStep)
+			}
 		}
 	}
-	return config, nil
+	return settings, nil
 }
 
 // 将默认配置写入settings.ini
@@ -109,8 +122,12 @@ func writeIniConfig(settingsPath string, config Settings) error {
 	// 生成配置文件内容
 	// 首行注释信息末尾加windows换行符
 	settingsContent := "; 根据注释修改配置，初始为默认设置，如设置值验证失败将采用默认值。\r\n"
-	settingsContent += "; subscribeURL 订阅地址\r\n"
+	settingsContent += "; 必填项（除非修改了开始步骤）：subscribeURL 订阅地址\r\n"
 	settingsContent += fmt.Sprintf("subscribeURL = %s\r\n", config.SubscribeURL)
+	settingsContent += ";必填项：templatePath 模板文件路径\r\n"
+	settingsContent += fmt.Sprintf("templatePath = %s\r\n", config.TemplatePath)
+	settingsContent += ";outputPath 输出文件路径，默认值为程序目录下“output.json”\r\n"
+	settingsContent += fmt.Sprintf("outputPath = %s\r\n", config.OutputPath)
 	settingsContent += "; base64File 获取到的节点列表base64加密文件\r\n"
 	settingsContent += "; 如果文件路径配置并存在将跳过从远程连接获取信息\r\n"
 	settingsContent += fmt.Sprintf("Base64File = %s\r\n", config.Base64File)
@@ -118,14 +135,11 @@ func writeIniConfig(settingsPath string, config Settings) error {
 	settingsContent += fmt.Sprintf("tempListPath = %s\r\n", config.TempListPath)
 	settingsContent += "; tempJsonPath 临时JSON文件路径\r\n"
 	settingsContent += fmt.Sprintf("tempJsonPath = %s\r\n", config.TempJsonPath)
-	settingsContent += "; templatePath 模板文件路径\r\n"
-	settingsContent += fmt.Sprintf("templatePath = %s\r\n", config.TemplatePath)
 	settingsContent += "; excludeKeywords 排除关键词\r\n"
 	settingsContent += fmt.Sprintf("excludeKeywords = %s\r\n", config.Filter[0].Keywords[0])
-	settingsContent += "; outputPath 输出文件路径\r\n"
-	settingsContent += fmt.Sprintf("outputPath = %s\r\n", config.OutputPath)
 	settingsContent += "; startStep 开始步骤,1.远程订阅地址，2.读取base64File，3.读取临时列表，\r\n"
 	settingsContent += "; 4.读取临时json，后续步骤由于会缺少数据所以大于等于4的步骤都会从第4步开始。\r\n"
+	settingsContent += "; 修改步骤后请确保相应数据文件路径配置正确，否则会因为无数据出错。\r\n"
 	settingsContent += fmt.Sprintf("startStep = %d\r\n", config.StartStep)
 	// 写入配置文件
 	if err := os.WriteFile(settingsPath, []byte(settingsContent), 0644); err != nil {
